140fd74c46bc7a246a441b89d863a0c066641195
Ian <typppo@gmail.com>
2014-02-23 01:20:36 +0000

    accept/reject logic

--- web/js/txn.js
+++ web/js/txn.js
@@ -4,10 +4,12 @@ Parse.initialize("eYl06p1bCOWl7oInD4z6MTtNuNJGkAeC8vRWMB3b", "WMyuYm1Wmb9NtbzkgN
 
 var Transaction = Parse.Object.extend("Transaction");
 
+var txn_id;
 if (!document.location.search || document.location.search.length < 2) {
   alert('Invalid transaction id.');
 } else {
-  load_txn(document.location.search.slice(1));
+  txn_id = document.location.search.slice(1);
+  load_txn();
 }
 
 function load_txn(txn_id) {
@@ -25,3 +27,44 @@ function load_txn(txn_id) {
     }
   });
 }
+
+function accept_request() {
+  var txn = new Transaction();
+  txn.save({
+    id: txn_id,
+    state: 'PENDING_MEETUP'
+  }, {
+    success: function() {
+      alert('You got it!  Your guest has been notified.');
+    },
+    error: function(obj, err) {
+      alert("Error :( " + err.message);
+    }
+  });
+
+  return false;
+}
+
+function reject_request() {
+  var txn = new Transaction();
+  txn.save({
+    id: txn_id,
+    state: 'REJECTED'
+  }, {
+    success: function() {
+      alert('The request has been rejected.');
+    },
+    error: function(obj, err) {
+      alert("Error :( " + err.message);
+    }
+  });
+
+  return false;
+
+  return false;
+}
+
+$(function() {
+  $('#accept_request').on('click', accept_request);
+  $('#reject_request').on('click', reject_request);
+});
